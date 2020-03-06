8de4e83a250c0a5269c2ae5f11a76a2fcb3958cf
Daniel Bicho <daniel.bicho@fccn.pt>
2020-03-06 14:40:56 +0000

    Default WARCParserCommand to publish to Solr and add some fields

--- warc-parser/src/main/java/org/arquivo/parsers/WARCParserCommand.java
+++ warc-parser/src/main/java/org/arquivo/parsers/WARCParserCommand.java
@@ -4,6 +4,9 @@ import com.typesafe.config.Config;
 import com.typesafe.config.ConfigFactory;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.apache.solr.client.solrj.SolrClient;
+import org.apache.solr.client.solrj.SolrServerException;
+import org.apache.solr.client.solrj.impl.HttpSolrClient;
 import org.archive.io.ArchiveReader;
 import org.archive.io.ArchiveReaderFactory;
 import org.archive.io.ArchiveRecord;
@@ -11,14 +14,17 @@ import org.arquivo.solr.SolrDocumentWrapper;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.List;
 
 public class WARCParserCommand {
 
     private static Log log = LogFactory.getLog(WARCParser.class);
 
-    public static void processArchive(WARCParser warcParser, File file) {
+    public static ArrayList<SolrDocumentWrapper> processArchive(WARCParser warcParser, File file) {
         ArchiveReader reader = null;
+        ArrayList<SolrDocumentWrapper> listDocs = new ArrayList();
         try {
             reader = ArchiveReaderFactory.get(file);
             Iterator<ArchiveRecord> ir = reader.iterator();
@@ -31,12 +37,10 @@ public class WARCParserCommand {
                     ArchiveRecord rec = ir.next();
                     try {
                         SolrDocumentWrapper doc = warcParser.extract("teste", rec);
-
-                        // move this code
-                        System.out.println(doc.getSolrInputDocument());
-                        // solrClient.add(doc.getSolrInputDocument());
-                        // solrClient.commit();
-
+                        if (doc != null){
+                            listDocs.add(doc);
+                        }
+                        log.debug(doc.getSolrInputDocument());
                     } catch (Exception e) {
                         continue;
                     } catch (OutOfMemoryError e) {
@@ -54,10 +58,24 @@ public class WARCParserCommand {
         } catch (IOException e) {
             e.printStackTrace();
         }
-
+        return listDocs;
     }
 
-    public static void main(String[] args) {
+    public static void publishToSolr(ArrayList<SolrDocumentWrapper> list, Config conf) throws IOException, SolrServerException {
+        final String SOLR_URL = conf.getString("warc.solr.server");
+
+        SolrClient solrClient = new HttpSolrClient.Builder(SOLR_URL)
+                .withConnectionTimeout(10000)
+                .withSocketTimeout(60000)
+                .build();
+
+        for (SolrDocumentWrapper doc : list){
+            solrClient.add(doc.getSolrInputDocument());
+        }
+        solrClient.commit();
+    }
+
+    public static void main(String[] args) throws IOException, SolrServerException {
 
         Config conf = ConfigFactory.load();
         WARCParser warcParser = new WARCParser(conf);
@@ -76,7 +94,8 @@ public class WARCParserCommand {
                         String fileName = child.getName();
                         if (fileName.endsWith("warc.gz") || fileName.endsWith("arc.gz") || fileName.endsWith("warc") ||
                                 fileName.endsWith("arc")) {
-                            processArchive(warcParser, child);
+                            ArrayList<SolrDocumentWrapper> listDocs = processArchive(warcParser, child);
+                            publishToSolr(listDocs, conf);
                         }
                     }
                 }
