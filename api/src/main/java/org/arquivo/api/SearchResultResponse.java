09a16d44ffbd722c758a7aab5b76216f3e1e57fb
Daniel Bicho <daniel.bicho@fccn.pt>
2020-03-10 17:18:56 +0000

    add SearchResultResponse

---
+++ api/src/main/java/org/arquivo/api/SearchResultResponse.java
@@ -0,0 +1,81 @@
+package org.arquivo.api;
+
+import org.arquivo.services.SearchResult;
+
+import java.util.ArrayList;
+
+public class SearchResultResponse {
+
+    private String serviceName;
+    private String linkToService;
+    private String nextPage;
+    private String previousPage;
+    private long totalItems;
+    private String estimatedNumberResults;
+    private SearchQuery requestParameters;
+
+    private ArrayList<SearchResult> responseItems;
+
+    public String getServiceName() {
+        return serviceName;
+    }
+
+    public void setServiceName(String serviceName) {
+        this.serviceName = serviceName;
+    }
+
+    public String getLinkToService() {
+        return linkToService;
+    }
+
+    public void setLinkToService(String linkToService) {
+        this.linkToService = linkToService;
+    }
+
+    public String getNextPage() {
+        return nextPage;
+    }
+
+    public void setNextPage(String nextPage) {
+        this.nextPage = nextPage;
+    }
+
+    public String getPreviousPage() {
+        return previousPage;
+    }
+
+    public void setPreviousPage(String previousPage) {
+        this.previousPage = previousPage;
+    }
+
+    public long getTotalItems() {
+        return totalItems;
+    }
+
+    public void setTotalItems(long totalItems) {
+        this.totalItems = totalItems;
+    }
+    public String getEstimatedNumberResults() {
+        return estimatedNumberResults;
+    }
+
+    public void setEstimatedNumberResults(String estimatedNumberResults) {
+        this.estimatedNumberResults = estimatedNumberResults;
+    }
+
+    public SearchQuery getRequestParameters() {
+        return requestParameters;
+    }
+
+    public void setRequestParameters(SearchQuery requestParameters) {
+        this.requestParameters = requestParameters;
+    }
+
+    public ArrayList<SearchResult> getResponseItems() {
+        return responseItems;
+    }
+
+    public void setResponseItems(ArrayList<SearchResult> responseItems) {
+        this.responseItems = responseItems;
+    }
+}
