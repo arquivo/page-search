291f0ed2785d8c062cf02266165450b11a099f4a
Daniel Bicho <daniel.bicho@fccn.pt>
2020-06-06 12:15:31 +0100

    Configurable Search Backend

--- page-search-api/src/main/java/pt/arquivo/api/PageSearchApplication.java
+++ page-search-api/src/main/java/pt/arquivo/api/PageSearchApplication.java
@@ -1,5 +1,8 @@
 package pt.arquivo.api;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
@@ -8,11 +11,17 @@ import org.springframework.context.annotation.Bean;
 import pt.arquivo.services.SearchService;
 import pt.arquivo.services.cdx.CDXSearchService;
 import pt.arquivo.services.nutchwax.NutchWaxSearchService;
+import pt.arquivo.services.solr.SolrSearchService;
 
 import java.io.IOException;
 
 @SpringBootApplication
 public class PageSearchApplication extends SpringBootServletInitializer {
+
+    private static final Logger LOG = LoggerFactory.getLogger(PageSearchApplication.class);
+
+    @Value("${searchpages.textsearch.service.bean}")
+    private String searchServiceBackend;
 
     @Bean
     CDXSearchService generateCDXSearchService() throws IOException {
@@ -21,7 +30,14 @@ public class PageSearchApplication extends SpringBootServletInitializer {
 
     @Bean
     SearchService generateService() throws IOException {
-        // return new SolrSearchService();
+        if (searchServiceBackend.equalsIgnoreCase("nutchwax")) {
+            LOG.info("Loading Nutchwax Search Service backend...");
+            return new NutchWaxSearchService();
+        }
+        if (searchServiceBackend.equalsIgnoreCase("solr")) {
+            LOG.info("Loading Solr Search Service backend...");
+            return new SolrSearchService();
+        }
         return new NutchWaxSearchService();
     }
 
@@ -30,7 +46,7 @@ public class PageSearchApplication extends SpringBootServletInitializer {
     }
 
     @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder application){
-       return application.sources(PageSearchApplication.class);
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(PageSearchApplication.class);
     }
 }
