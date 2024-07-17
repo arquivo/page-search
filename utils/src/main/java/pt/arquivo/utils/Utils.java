13fdf4afb97ebe66f2ccb8a532f51956e9b7111f
Vasco Rato <vasco.rato@fccn.pt>
2024-07-17 17:18:31 +0100

    Added basic date verification to canocalizeTimestamp

--- utils/src/main/java/pt/arquivo/utils/Utils.java
+++ utils/src/main/java/pt/arquivo/utils/Utils.java
@@ -24,6 +24,39 @@ public class Utils {
         } else if(timestamp.length() < 14) {
             r = timestamp + "19960101000000".substring(timestamp.length());
         }
+        Integer year, month, day, hour, minute, second;
+        
+        year = Integer.parseInt(r.substring(0,4));
+        month = Integer.parseInt(r.substring(4,6));
+        day = Integer.parseInt(r.substring(6,8));
+        hour = Integer.parseInt(r.substring(8,10));
+        minute = Integer.parseInt(r.substring(10,12));
+        second = Integer.parseInt(r.substring(12,14));
+        if(year < 1996 || year > 9999){
+            year = 1996;
+        }
+        if(month < 1 || month > 12){
+            month = 1;
+        }
+        if(day < 1 || day > 31){
+            day = 1;
+        }
+        if(hour > 23){
+            hour = 0;
+        }
+        if(minute > 59){
+            minute = 0;
+        }
+        if(second > 59){
+            second = 0;
+        }
+
+        r = String.format("%04d", year)
+        + String.format("%02d", month)
+        + String.format("%02d", day)
+        + String.format("%02d", hour)
+        + String.format("%02d", minute)
+        + String.format("%02d", second);;
         return r;
     }
 
